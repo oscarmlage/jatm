a6426bb83c15f7f5a52f062f511abc6cf0cab938
Óscar M. Lage <info@oscarmlage.com>
2021-09-17 00:48:09 +0200

    Add: env-example.py added.

---
+++ env-example.py
@@ -0,0 +1,15 @@
+import os
+
+# Move this file to env.py
+
+# secrets
+
+os.environ['VERSION'] = '0.0.1'
+# Register new app here: https://dev.twitch.tv/console/apps/create
+os.environ['CLIENT_ID'] = 'YOUR-APP-CLIENT-ID'
+# Outh token here: https://twitchapps.com/tmi/
+os.environ['TOKEN'] = 'oauth:YOUR-TOKEN'
+os.environ['BOT_NICK'] = 'YOURNICK'
+os.environ['BOT_PREFIX'] = '!'
+os.environ['CHANNEL'] = '#YOURCHANNEl'
+
